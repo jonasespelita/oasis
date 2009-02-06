fab6cab65c63fcc3874a466e59575b119675916c
Jonas Espelita <jonasespelita@gmail.com>
2009-02-07 01:30:50 +0800

    Final fixes to conflicts...success merging with ayo!
    Signed-off-by: Jonas Espelita <jonasespelita@gmail.com>

--- oasisapp/app/controllers/users_controller.rb
+++ oasisapp/app/controllers/users_controller.rb
@@ -28,9 +28,9 @@ class UsersController < ApplicationController
       profile = Profile.find(session[:stud_idno])
       flash[:notice] = "#{profile.fullname}"
       render :action => "finish"
-     
+    else
+      render :action => "new"
     end
-    render :action => "new"
   end
   
   def finish
@@ -39,7 +39,6 @@ class UsersController < ApplicationController
   def change_password
     @user = current_user
     @old = params[:old_password]
-
     @lang = case current_user.lang_pref
     when 1
      "<option selected='selected'>English</option> <option>Filipino</option>"
@@ -49,39 +48,50 @@ class UsersController < ApplicationController
 
     @checkemail = current_user.email_pref
     @checkcp = current_user.mobile_pref
-
   end
 
-
-  #
-  # Change user passowrd
   def change_password_update
     if User.authenticate(current_user.login, params[:old_password])
       if ((params[:password] == params[:password_confirmation]) && !params[:password_confirmation].blank?)
         current_user.password_confirmation = params[:password_confirmation]
         current_user.password = params[:password]
 
-        if current_user.save!
-          flash[:notice] = "Password successfully updated"
-          redirect_to change_password_path
+        if current_user.save
+          flash[:alert] = "Password successfully changed"
         else
           flash[:alert] = "Password not changed"
-          render :action => 'change_password'
         end
 
       else
         flash[:alert] = "New Password mismatch"
-        render :action => 'change_password'
       end
     else
       flash[:alert] = "Old password incorrect"
-      render :action => 'change_password'
+    end
+
+    respond_to do |format|
+      format.html{render :action => 'change_password'}
+      format.js
+      
+    end
+  end
+
+  def change_profile_update
+    current_user.nickname = params[:n_name]
+    current_user.address = params[:address]
+
+    if current_user.save
+      flash[:name_alert] = "Profile Updated!"
+    else
+      flash[:name_alert] = "Invalid inputs."
+    end
+    respond_to do |format|
+      format.js
     end
   end
 
   def change_email
     current_user.email = params[:email]
-
     current_user.cp_number = params[:cp]
     current_user.lang_pref = 2
     current_user.mobile_pref = params[:val2]
@@ -94,11 +104,12 @@ class UsersController < ApplicationController
 #    end
     
     if current_user.save
-      flash[:e_alert] = "Settings successfully updated}"
-
+      flash[:e_alert] = "Settings successfully updated"
     else
-      flash[:alert] = "Invalid Email"
-      render :action => 'change_password'
+      flash[:e_alert] = "Invalid Input."
+    end
+    respond_to do |format|
+      format.js
     end
   end
 
@@ -107,28 +118,39 @@ class UsersController < ApplicationController
     
   end
 
-  def vlogin
 
-      if "#{params[:login]}".length==0
-         render :text => "Your desired username."
-         return
-      end
+  ## validation methods
+  def vlogin
+    if !("#{params[:login]}" =~ /\A[A-Za-z_0-9]*\Z/)
+      render :text => "Special characters are not allowed."
+      return
+    end
+
+    if "#{params[:login]}".length==0
+      render :text => "Your desired username."
+      return
+    end
+    if "#{params[:login]}".length < 6
+      render :text => "Username is too short. Use 6 to 32 characters"
+      return
+    end
       
     if(User.find_by_login params[:login]  )
    
-      render :text => "Username has already been taken"
+      render :text => "Username already taken."
         
-      else
-     
- 
-      render :text =>"Ok!"
+    else
+    
+      render :text =>"<div class='OkNa'>Ok!</div>"
     end
   end
 
+
   def vpassword
+    session[:password] = params[:password]
     if("#{params[:password]}".length >=6)
-      session[:password] = params[:password]
-      render :text => "Ok!"
+      
+      render :text => "<div class='OkNa'>Ok!</div>"
     else
       render :text => "Use 6 to 32 characters, no spaces."
     end
@@ -137,28 +159,93 @@ class UsersController < ApplicationController
 
   def vconfirm_password
 
-    if params[:confirm_password]== session[:password]
-      render :text => "Ok!"
+    if "#{params[:confirm_password]}".length == 0
+      render :text => "Please enter your chosen password again."
+      
     else
-      render :text => "Passwords don't match!"
+      render :text => "Capitalization matters!"
     end
   end
 
   def vemail
     if params[:email] =~ /(.+)@(.+)\.(.{3})/
       if !User.find_by_email(params[:email])
-      render :text => "Ok!"
+        render :text => "<div class='OkNa'>Ok!</div>"
       else
         render :text => "Email has already been registered. Choose another."
       end
 
-     else
-       render :text => "Invalid Email address."
-     end
+    else
+      if "#{params[:email]}".length==0
+        render:text => ""
+        return
+      end
+      render :text => "Invalid Email address."
+    end
+  end
+  def vname
+    if !("#{params[:name]}" =~ /\A[A-Za-z\s]*\Z/)
+      render :text => "Special characters are not allowed."
+      return
+    else
+      if "#{params[:name]}".length!=0
+        render :text => "<div class='OkNa'>Ok!</div>"
+      else
+        render :text => ""
+      end
+    end
+  end
 
+  def vnickname
+    if !("#{params[:nickname]}" =~ /\A[A-Za-z\-\s]*\Z/)
+      render :text => "Special characters are not allowed."
+      return
+    else
+      if "#{params[:nickname]}".length < 2
+        render :text => "Nickname is too short. Use 2 or more characters."
+        return
+      end
+      if "#{params[:nickname]}".length!=0
+        render :text => "<div class='OkNa'>Ok!</div>"
+      else
+        render :text => ""
+      end
+    end
+  end
 
+  def vcp_number
+    if "#{params[:cp_number]}".length ==0;
+      render :text => ""
+      return
+    end
+    if params[:cp_number] =~ /\A([\+]?(63[0-9]{10}\Z)|(0[0-9]{10})*\Z)/
+      render :text => "<div class='OkNa'>Ok!</div>"
+    else
+     
+      render :text => "Invalid mobile number."
+    end
 
   end
 
+  def vaddress
+    if !("#{params[:address]}" =~ /\A[A-Za-z_0-9\s\#.\-]*\Z/)
+      render :text=> "Invalid address."
+    else
+      if "#{params[:address]}".length == 0
+        render :text => "Enter your home address."
+        return
+      end
+      render :text => "<div class='OkNa'>Ok!</div>"
+    end
+  end
 
+  def vold_password
+    if current_user.authenticated? params[:password]
+      render :text => "<div class='OkNa'>Ok!</div>"
+
+    else
+      render :text =>"Your old password"
+    end
+
+  end
 end
